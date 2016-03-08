70a923ca45e65b1e00461dd370c4cec6b44f7763
John-Michael L'Allier <jmlallier@me.com>
2016-03-08 18:42:05 -0500

    Create articles table, model, and add validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308223628) do
+ActiveRecord::Schema.define(version: 20160308231753) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
