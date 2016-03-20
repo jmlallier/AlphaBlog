7e5ae6ff43a0fb426c3e1bd66fe51cb54c8e981d
John-Michael L'Allier <jmlallier@me.com>
2016-03-20 18:32:34 -0400

    add authentication for users, show specific navigation if logged_in or\!logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,17 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    @current_user = User.find(session[:user_id]) if session[:user_id] # find and return user by user_id if there is a user_id stored in session
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] # find and return user by user_id if there is a user_id stored in session
   end
 
   def logged_in?
-
+    !!current_user
   end
 
   def require_user
-
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform this action"
+      redirect_to login_path
+    end
   end
 end
