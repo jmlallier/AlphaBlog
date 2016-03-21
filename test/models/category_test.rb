55512255962c76293bd3e20b056b65272499189f
John-Michael L'Allier <jmlallier@me.com>
2016-03-21 19:38:20 -0400

    add category model and db, add testing and validation for category name

---
+++ test/models/category_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  
+  def setup
+    
+    @category = Category.new(name: 'travel')
+    
+  end
+  
+  test "category should be valid" do
+    
+    assert @category.valid?
+    
+  end
+  
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+  
+  test "name should be unique" do 
+    @category.save
+    category2 = Category.new(name: 'travel')
+    assert_not category2.valid?
+  end
+  
+  test "name should not be too long" do
+    @category.name = "a" * 36
+    assert_not @category.valid?
+  end
+  
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+  
+  
+  
+end
