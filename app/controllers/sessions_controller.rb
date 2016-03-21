d30f456f99a4fff47eb5f5718b8e3cce46f99c29
John-Michael L'Allier <jmlallier@me.com>
2016-03-21 16:00:05 -0400

    remove bootstrap-will_paginate styling of pagination; change login redirect to previous page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
+  
   def new
-    
+    session[:return_to] ||= request.referer
   end
   
   def create
@@ -9,7 +10,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id # store :user_in browser session
       flash[:success] = "You have successfully been logged in" #tell user they are logged in
-      redirect_to user_path(user) # take user to their page
+      redirect_to session.delete(:return_to) # take user to their page
     else
       flash.now[:danger] = "The email or password you entered did not match our records. Please try again." # oops, error!
       render 'new' # re-render login page
