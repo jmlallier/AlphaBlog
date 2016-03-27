dc9defe6e63748375f877fbdccdc95aff50cd900
John-Michael L'Allier <jmlallier@me.com>
2016-03-27 13:06:51 -0400

    Add edit category feature to UI

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,6 +19,20 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "The category has been successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
   
   def show
     @category = Category.find(params[:id])
